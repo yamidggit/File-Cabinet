8f4abce345b6a4f77d867330c4f403bd74ebdf1b
Yamilet <yamidg@yahoo.com>
2016-07-18 18:23:41 +0000

    change root of app for authenticated user

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
   
-  root 'welcome#index'
   resources :docs
+  authenticated :user do
+    root "docs#index", as: "authenticated_root" # this is another root in case that user sign in, they can see their docs and not all the docs
+  end
+  root 'welcome#index'
 end
